5f72eb995f28ea2812519846ef52a5e0b9432386
Paul Tang <cftang0827@gmail.com>
2019-06-14 14:37:36 +0800

    first version

---
+++ setup.py
@@ -0,0 +1,14 @@
+# coding:utf-8
+
+from setuptools import setup
+# or
+# from distutils.core import setup
+
+setup(
+    name='simple_timer',
+    version='1.0',
+    description='This is a Python based simple timer',
+    author='cftang',
+    author_email='cftang0827@gmail.com',
+    packages=['simple_timer'],
+)
---
+++ simple_timer/timer.py
@@ -0,0 +1,14 @@
+import timeit
+
+
+class Timer:
+
+    def __init__(self, task_name):
+        self.task_name = task_name
+
+    def __enter__(self):
+        self.t1 = timeit.default_timer()
+
+    def __exit__(self, type, value, trace):
+        print(timeit.default_timer() - self.t1)
+        print("The overall time of {}: {:3f} secs".format(self.task_name, timeit.default_timer() - self.t1))
